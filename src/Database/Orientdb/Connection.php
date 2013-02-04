9fab07df5429b2cf84ba2e392371fcc03d5213b0
Yuta Sakurai <sakurai.yuta@gmail.com>
2013-02-04 20:04:16 +0900

    first import

---
+++ src/Database/Orientdb/Connection.php
@@ -0,0 +1,204 @@
+<?php
+
+use Doctrine\OrientDB\Binding\BindingParameters;
+use Doctrine\OrientDB\Binding\HttpBinding;
+use Doctrine\OrientDB\Query\Query;
+use Doctrine\OrientDB\Query\Validator\Escaper;
+use Doctrine\ODM\OrientDB\Manager;
+use Doctrine\ODM\OrientDB\Mapper;
+use Doctrine\ODM\OrientDB\Mapper\Annotations\Reader;
+use Doctrine\Common\Cache\ArrayCache;
+
+class Database_Orientdb_Connection extends \Fuel\Core\Database_Connection
+{
+
+	/**
+	 * @var  string  OrientDB identifiers
+	 */
+	protected $_identifier = '';
+
+	protected $_escaper;
+
+	protected function __construct($name, array $config)
+	{
+		parent::__construct($name, $config);
+		$this->_escaper = new Escaper();
+	}
+
+	public function connect()
+	{
+		$proxy_dir = APPPATH . "vendor" . DS . "sakuraiyuta" . DS . "fuel-orientdb" . DS . "tmp";
+
+		if ($this->_connection)
+		{
+			return;
+		}
+
+		// parse dsn
+		$config = $this->_config['connection'];
+		$dsn = $this->_config["connection"]["dsn"];
+		$dsn_temp = explode(':', $dsn);
+		$this->_db_type = $dsn_temp[0];
+		foreach ( explode(';', $dsn_temp[1]) as $row )
+		{
+			$row_temp = explode('=', $row);
+			$config[$row_temp[0]] = $row_temp[1];
+		}
+
+		$parameters = BindingParameters::create(
+			"http://{$config['username']}:{$config['password']}@{$config['host']}:{$config['port']}/{$config['dbname']}"
+		);
+		$binding = new HttpBinding($parameters);
+		$reader = new Reader(new ArrayCache());
+		$mapper = new Mapper($proxy_dir, $reader);
+		$mapper->setDocumentDirectories(
+			array(APPPATH . "classes" . DS . "model")
+		);
+		$manager = new Manager($mapper, $binding);
+
+		// Prevent this information from showing up in traces
+		unset($config['username'], $config['password']);
+
+		$this->_connection = array(
+			"manager" => $manager,
+			"binding" => $binding,
+		);
+	}
+
+	public function disconnect()
+	{
+		//TODO: implement
+		throw new Exception("not implemented yet.");
+
+//		try
+//		{
+//			// Database is assumed disconnected
+//			$status = true;
+//
+//			if ($this->_connection instanceof \MySQLi)
+//			{
+//				$status = $this->_connection->close();
+//			}
+//		}
+//		catch (\Exception $e)
+//		{
+//			// Database is probably not disconnected
+//			$status = ! ($this->_connection instanceof \MySQLi);
+//		}
+//
+//		return $status;
+	}
+
+	public function set_charset($charset)
+	{
+		//TODO: implement
+		throw new Exception("not implemented yet.");
+	}
+
+	public function query($type, $sql, $as_object)
+	{
+		if ( ! $this->_connection )
+		{
+			$this->connect();
+		}
+
+		if ( ! empty($this->_config['profiling']))
+		{
+			// Benchmark this query for the current instance
+			$benchmark = \Profiler::start("Database ({$this->_instance})", $sql);
+		}
+
+		// Execute the query
+		try {
+			if ( ! $sql instanceof Query )
+			{
+				$result = $this->_connection["binding"]->query($sql);
+			}
+			else
+			{
+				$result = $this->_connection["manager"]->execute($sql);
+			}
+		} catch (Exception $e) {
+			if (isset($benchmark))
+			{
+				// This benchmark is worthless
+				\Profiler::delete($benchmark);
+			}
+
+			throw $e;
+		}
+
+		if (isset($benchmark))
+		{
+			\Profiler::stop($benchmark);
+		}
+
+		// Set the last query
+		$this->last_query = $sql;
+
+		if ($type === \DB::SELECT)
+		{
+			return $result->getResult();
+			// Return an iterator of results
+//			return new \Database_MySQLi_Result($result, $sql, $as_object);
+		}
+		elseif ($type === \DB::INSERT)
+		{
+			//TODO: implement when inserts record
+			throw new Exception("not implemented yet.");
+//			// Return a list of insert id and rows created
+//			return array(
+//				$this->_connection->insert_id,
+//				$this->_connection->affected_rows,
+//			);
+		}
+		else
+		{
+			//TODO implement returning affected rows
+			throw new Exception("not implemented yet.");
+			// Return the number of rows affected
+//			return $this->_connection->affected_rows;
+		}
+	}
+
+	public function list_tables($like = null)
+	{
+		//TODO: implement
+		throw new Exception("not implemented yet.");
+	}
+
+	public function list_columns($table, $like = null)
+	{
+		//TODO: implement
+		throw new Exception("not implemented yet.");
+	}
+
+	public function escape($value)
+	{
+		return $this->_escaper->check($value);
+	}
+
+	public function in_transaction()
+	{
+		//TODO: implement
+		throw new Exception("not implemented yet.");
+	}
+
+	public function start_transaction()
+	{
+		//TODO: implement
+		throw new Exception("not implemented yet.");
+	}
+
+	public function commit_transaction()
+	{
+		//TODO: implement
+		throw new Exception("not implemented yet.");
+	}
+
+	public function rollback_transaction()
+	{
+		//TODO: implement
+		throw new Exception("not implemented yet.");
+	}
+}
