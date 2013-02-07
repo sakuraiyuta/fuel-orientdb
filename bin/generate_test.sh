f785d810f8907ed7d1c3e15af2b35b2490ef7b8a
Yuta Sakurai <sakurai.yuta@gmail.com>
2013-02-07 10:19:06 +0900

    create and modify phpunit test configurations.

--- bin/generate_test.sh
+++ bin/generate_test.sh
@@ -6,9 +6,11 @@ if [ -z ${1} ]; then
 fi
 
 TARGET_PHP_FILE=${1}
+BOOTSTRAP="$(dirname ${0})/../../../../../../public/index.php"
 CLASS_NAME=`echo ${1} | sed "s%^.*/\([^/]*\).php$%\1%g"`
-TEST_PATH=`echo ${1} | sed "s%^src%test%g" | sed "s%\.php$%Test\.php%g"`
+TESTCLASS_NAME="${CLASS_NAME}Test"
+TEST_FILE="$(dirname ${0})/../test/${TESTCLASS_NAME}.php"
 
 echo "generating test: ${TARGET_PHP_FILE} > ${CLASS_NAME} > ${TEST_PATH}"
-
-phpunit-skelgen --bootstrap ../../../../../public/index.php --test -- ${CLASS_NAME} ${TARGET_PHP_FILE} tests/
+echo "phpunit-skelgen --bootstrap ${BOOTSTRAP} --test -- ${CLASS_NAME} ${TARGET_PHP_FILE} ${TEST_FILE}"
+phpunit-skelgen --bootstrap ${BOOTSTRAP} --test -- ${CLASS_NAME} ${TARGET_PHP_FILE} ${TESTCLASS_NAME} ${TEST_FILE}
