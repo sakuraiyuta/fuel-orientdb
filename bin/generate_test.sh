e934c82589dbefb7c45f388d023ccdb3e979fa55
Yuta Sakurai <sakurai.yuta@gmail.com>
2013-02-07 11:03:34 +0900

    modify testclass generater.

--- bin/generate_test.sh
+++ bin/generate_test.sh
@@ -5,12 +5,20 @@ if [ -z ${1} ]; then
 	exit
 fi
 
-TARGET_PHP_FILE=${1}
-BOOTSTRAP="$(dirname ${0})/../../../../../../public/index.php"
-CLASS_NAME=`echo ${1} | sed "s%^.*/\([^/]*\).php$%\1%g"`
-TESTCLASS_NAME="${CLASS_NAME}Test"
-TEST_FILE="$(dirname ${0})/../test/${TESTCLASS_NAME}.php"
+export doc_root=../../../.. app_path=../../.. core_path=../../../../core package_path=../../../../packages FUEL_ENV=test
 
-echo "generating test: ${TARGET_PHP_FILE} > ${CLASS_NAME} > ${TEST_PATH}"
-echo "phpunit-skelgen --bootstrap ${BOOTSTRAP} --test -- ${CLASS_NAME} ${TARGET_PHP_FILE} ${TEST_FILE}"
+BOOTSTRAP="$(dirname ${0})/../bootstrap_phpunit.php"
+TARGET_PHP_FILE=$(cd $(dirname "${1}") && pwd)/$(basename ${1})
+CLASS_NAME=`echo $(basename "${TARGET_PHP_FILE}") | sed "s%\(.*\).php$%\1%"`
+
+TESTCLASS_NAME="${CLASS_NAME}Test"
+TEST_PATH=`echo "${TARGET_PHP_FILE}" | sed 's%/src/%/test/%' | sed 's%[^/]*.php$%%'`
+TEST_FILE="${TEST_PATH}/${TESTCLASS_NAME}.php"
+
+if [ ! -d "${TEST_PATH}" ]; then
+	mkdir -p "${TEST_PATH}"
+fi
+
+echo "generating test: ${TARGET_PHP_FILE} > ${CLASS_NAME} > ${TEST_FILE}"
+#echo phpunit-skelgen --bootstrap ${BOOTSTRAP} --test -- ${CLASS_NAME} ${TARGET_PHP_FILE} ${TESTCLASS_NAME} ${TEST_FILE}
 phpunit-skelgen --bootstrap ${BOOTSTRAP} --test -- ${CLASS_NAME} ${TARGET_PHP_FILE} ${TESTCLASS_NAME} ${TEST_FILE}
